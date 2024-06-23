7431d9aa84f1ed152e07a711c431c5ebd396de46
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-06-23 07:54:21 +0100

    adding the global axios function

---
+++ src/utils/axios.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_API_URL,
+});
+
+let csrfToken = null;
+let isFetchingToken = false;
+
+const fetchCsrfToken = async () => {
+  try {
+    if (!csrfToken && !isFetchingToken) {
+      isFetchingToken = true;
+      const response = await axiosInstance.get('/request-token'); 
+      csrfToken = response.data.csrfToken;
+      Cookies.set('csrftoken', csrfToken); // Store the token in cookies if needed
+      isFetchingToken = false;
+    }
+  } catch (error) {
+    console.error('Error fetching CSRF token:', error);
+    isFetchingToken = false;
+  }
+};
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (!csrfToken) {
+      await fetchCsrfToken();
+    }
+    config.headers['X-CSRFToken'] = csrfToken;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
--- src/utils/csrftoken.js
+++ src/utils/csrftoken.js
@@ -1,11 +1,23 @@
-export const getCsrfToken = async () => {
-  // const originUrl
-  try {
-    const response = await fetch('/api/csrf-token');
-    const data = await response.json();
-    return data.csrfToken;
-  } catch (error) {
-    console.error('Failed to fetch CSRF token:', error);
-    return null;
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_API_URL,
+});
+
+let csrfToken = null;
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (!csrfToken) {
+      csrfToken = Cookies.get('csrftoken');
+    }
+    config.headers['X-CSRF-Token'] = csrfToken;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-};
+);
+
+export default axiosInstance;
