600acdadde4157b51381dd203475e0258e5bda63
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-02 13:37:42 +0100

    added about us section

---
+++ src/pages/home/components/about/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SubHeader from 'components/typography/sub_header';
+
+// At Event Planner, we specialize in creating memorable events tailored to your unique needs. Our experienced team handles every detail, ensuring a seamless and stress-free experience for you. Whether you're planning a wedding, corporate event, or a social gathering, we bring your vision to life.
+const About = () => {
+  return (
+    <div className='flex flex-col items-start gap-[1rem] w-full'>
+      <SubHeader>About Us</SubHeader>
+
+      <div className='w-full '>
+      </div>
+    </div>
+  )
+}
+
+export default About
