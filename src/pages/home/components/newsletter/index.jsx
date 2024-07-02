4769763895c16c9b7f04bd75297b6443df32cc05
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-02 14:06:33 +0100

    finished newsletter

--- src/pages/home/components/newsletter/index.jsx
+++ src/pages/home/components/newsletter/index.jsx
@@ -7,14 +7,14 @@ import { Button } from '@mui/material';
 const Newsletter = () => {
   const [value, setValue] = useState('');
   return (
-    <div className='flex flex-col items-center gap-[1rem] w-full'>
+    <div className='flex flex-col items-center gap-[2rem] w-full'>
       <SubHeader>Sign up for our newsletter</SubHeader>
 
-      <div className='w-full '>
-        <p>Subscribe to our newsletter and stay updated on the latest events, exclusive offers, and more. Don't miss out on any exciting happenings!</p>
+      <div className='flex flex-col gap-[3rem] w-full '>
+        <p className='max-w-[35rem] mx-auto'>Subscribe to our newsletter and stay updated on the latest events, exclusive offers, and more. Don't miss out on any exciting happenings!</p>
 
         <form>
-          <div className='flex flex-col gap-[0.5rem] w-[20rem] mx-auto '>
+          <div className='flex flex-col gap-[0.5rem] w-full max-w-[25rem] mx-auto '>
             <label className='text-gray-500 text-[0.875rem] font-[600] w-full text-start'>Email:</label>
             <BaseInput 
               type='email'
@@ -29,7 +29,7 @@ const Newsletter = () => {
                 width: '100%',
                 backgroundColor: '#3F51B5',
                 borderRadius: '0.5rem',
-                padding: '0.5rem 1.5rem',
+                padding: '0.625rem 1.5rem',
                 '&:hover': {
                   backgroundColor: '#4963c7',
                 },
