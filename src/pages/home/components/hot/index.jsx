efdc1eed8a7fc834e61a1d55d0d8db5a81c6261d
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-02 15:29:48 +0100

    done with the home ui

--- src/pages/home/components/hot/index.jsx
+++ src/pages/home/components/hot/index.jsx
@@ -6,10 +6,10 @@ import SubHeader from 'components/typography/sub_header';
 const Hot = () => {
   return (
     <div className='flex flex-col items-start gap-[1rem] w-full'>
-      <SubHeader>Hot</SubHeader>
-
+      <SubHeader>Hot-Events</SubHeader>
+      <p className='text-[1.15rem] text-gray-500 font-[600]'> Don't Miss Out on These Must-See Experiences!</p>
       <div className='w-full overflow-hidden'>
-        <div className='grid grid-cols-1 xlg:grid-cols-2 gap-[1rem] gap-y-[3rem] w-full '>
+        <div className='grid grid-cols-1 xlg:grid-cols-2 gap-[2rem] lg:gap-[3rem] gap-y-[3rem] w-full '>
           {[...Array(4).keys()].map((_, index) => (
             <EventCard key={index} />
           ))}
