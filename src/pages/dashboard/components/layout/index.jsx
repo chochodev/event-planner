023e3469aacd1be0d5f2d9c80041f4a5ef73813b
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-03 18:55:00 +0100

    adding dashboard layout

--- src/pages/dashboard/components/layout/index.jsx
+++ src/pages/dashboard/components/layout/index.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
 import IconButton from '@mui/material/IconButton';
 import { 
   RiMenu5Line, 
@@ -12,43 +10,44 @@ import {
   RiUser3Line,
   RiCalendarEventLine,
   RiShoppingCartLine,
-  RiHistoryLine
+  RiHistoryLine,
+  RiSearch2Line,
+  RiNotification2Fill
 } from "react-icons/ri";
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { Button, Input, useMediaQuery } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
 import Logo from 'components/logo';
+import { Link } from 'react-router-dom';
 
 const DashboardLayout = ({ children }) => {
   const [openMenu, setOpenMenu] = useState(false);
-  const isLargeScreen = useMediaQuery('(min-width:600px)');
+  const isLargeScreen = useMediaQuery('(min-width:640px)');
 
   const navLinks = [
     {name: 'Dashboard', link: '/dashboard', icon: <RiDashboardHorizontalLine className='' />},
     {name: 'Profile', link: '/dashboard', icon: <RiUser3Line className='' />},
-    {name: 'My Events', link: '/dashboard/events', icon: <RiCalendarEventLine className='' />},
+    {name: 'Events', link: '/dashboard/events', icon: <RiCalendarEventLine className='' />},
     {name: 'Cart', link: '/dashboard', icon: <RiShoppingCartLine className='' />},
     {name: 'History', link: '/dashboard', icon: <RiHistoryLine className='' />},
     {name: 'Exit', link: '/dashboard', icon: <RiHome3Line className='' />},
   ]
 
+  // :::::::::::::::::::::: SEARCH
+  const [searchModal, setSearchModal] = useState(false);
+
+
   return (
     <div className='flex w-full'>
-      <div className="flex flex-col max-w-[75rem] mx-auto">
+      <div className="flex flex-col w-full">
         {/* ::::::::::::::::: HEAD */}
-        <div className='flex items-center w-full'>
-          <div>
-            <Logo />
+        <div className='flex items-center justify-between w-full max-w-[85rem] mx-auto h-[4rem] xmd:h-[5rem] '>
+          <div className='flex gap-[0.375rem] xmd:gap-[1rem] md:gap-[2rem] items-center pl-[1rem] xmd:pl-[2rem] '>
+            <Logo style='w-[3rem] xmd:w-[4rem]' />
 
-            {!useMediaQuery &&
+            {!isLargeScreen &&
             <IconButton
               onClick={()=>setOpenMenu(!openMenu)}
               sx={{
                 borderRadius: '10rem',
-                // padding: '0.5rem'
-
               }}
             >
               {!openMenu? 
@@ -57,25 +56,51 @@ const DashboardLayout = ({ children }) => {
               }
             </IconButton>}
 
-            <Input 
-              
-            />
+            <div className='relative flex items-center '>
+              <RiSearch2Line className='absolute right-[0.5rem] text-[1.15rem] text-gray-400 ' />
+              <input 
+                defaultValue=''
+                onClick={()=>setSearchModal(true)}
+                placeholder='Search'
+                className='h-[2.15rem] xmd:h-[2.5rem] w-[8rem] xmd:w-[12rem] text-[0.875rem] bg-gray-100 rounded-[8px] pl-[0.875rem] py-[0.25rem] '
+              />
+            </div>
           </div>
-          
 
-          <div>
-            <h2>Admin Dashboard</h2>
+          <div className='flex items-center gap-[1rem] pr-[1rem] xmd:pr-[2rem] '>
+            <h2 className='max-xmd:hidden text-[1.25rem] font-[600] text-black-dim'>Dashboard</h2>
+            <IconButton
+              sx={{
+                backgroundColor: '#00000010',
+                borderRadius: '8px'
+              }}
+            >
+              <RiNotification2Fill className='text-black-dim text-[1.15rem] ' />
+            </IconButton>
           </div>
         </div>
-        <div className='flex w-full'>
+
+        {/* :::::::::::::::: HORIZONTAL LINE */}
+        <hr className='w-full h-[1px] bg-gray-200' />
+          
+        <div className='hidden xmd:flex items-center h-[4rem] w-full overflow-x-hidden shadow-[0_5px_15px_rgba(0,0,0,0.1)] '>
           {/* ::::::::::::::::: NAV MENU */}
-          <nav>
-            
+          <nav className='flex gap-[1rem] md:gap-[1.5rem] max-w-[85rem] mx-auto '>
+            {navLinks.map((nav, index) => (
+              <Link 
+                key={index}
+                to={nav.link}
+                className='flex items-center gap-[0.25rem] h-[2rem] px-[1rem] text-[0.875rem] text-gray-500 hover:text-secondary-hover ease-250 '
+              >
+                {nav.icon}
+                <p>{nav.name}</p>
+              </Link>
+            ))}
           </nav>
           
-          {children}
 
         </div>
+          {children}
       </div>
       
     </div>
