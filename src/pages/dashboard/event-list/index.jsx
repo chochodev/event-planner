d1924ede399c3998b8e0519b08865ddf068646b7
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-06 19:30:43 +0100

    updating

--- src/pages/dashboard/event-list/index.jsx
+++ src/pages/dashboard/event-list/index.jsx
@@ -127,6 +127,7 @@ export default function MyEventList() {
                     sx={{
                       cursor: 'pointer'
                     }}
+                    onClick={()=>handleEventClick(event.id)}
                   >
                     <StyledTableCell>
                       <img 
