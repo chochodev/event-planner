840214ae0f0ab6ac5cae113db732c2ae09886cf8
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-08 23:19:51 +0100

    done with unauthorized page

--- src/components/error_page/unauthenticated/index.jsx
+++ src/components/error_page/unauthenticated/index.jsx
@@ -18,11 +18,11 @@ const AccessDenied = () => {
 
   return (
     <HomeLayout>
-      <section className="flex items-center justify-center w-full h-full min-h-[30rem] bg-primary py-[6rem]">
-        <div className="w-full h-full">
-          <h1 className="text-center font-secondary text-gray-500 text-[1.5rem] font-[600] mt-[-4rem]">Access Denied!</h1>
-          <div className="flex flex-col items-center text-center gap-[1rem] w-max mx-auto">
-            <h3 className="text-secondary-hover text-[2rem] font-[600]">Sign in to access this page</h3>
+      <section className="flex items-center justify-center w-full h-full min-h-[30rem] bg-primary py-[3rem] sm:py-[6rem] px-[1rem] ">
+        <div className="w-full max-w-full h-full">
+          <h1 className="text-center font-secondary text-gray-500 text-[1rem] sm:text-[1.5rem] font-[600] mt-[-4rem]">Access Denied!</h1>
+          <div className="flex flex-col items-center text-center gap-[1rem] w-full mx-auto">
+            <h3 className="text-wrap text-center text-secondary-hover text-[1.5rem] sm:text-[2rem] font-[600]">Sign in to access this page</h3>
             <p className="text-gray-600 text-[0.875rem] mb-[2rem] ">
               Redirecting to sign-in page in {seconds} seconds
             </p>
