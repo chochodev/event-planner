d90f72480edfb553621b61e9878406301b14c89b
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-11 08:46:57 +0100

    creating profile settings page

---
+++ src/pages/dashboard/profile/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardLayout from '../components/layout'
+
+const ProfileSetting = () => {
+  return (
+    <DashboardLayout>
+      <div>ProfileSetting</div>
+
+    </DashboardLayout>
+  )
+}
+
+export default ProfileSetting
