3d0649246c93676568120d5ae73186675ba63a17
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-11 09:58:11 +0100

    finished with the client side of the profile, I think

--- src/pages/dashboard/profile/index.jsx
+++ src/pages/dashboard/profile/index.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import DashboardLayout from '../components/layout';
 import axiosInstance from 'utils/axios';
 import FlashMessage from 'components/alert';
-import { Button, FormControl, MenuItem, Select, TextField } from '@mui/material';
+import { FormControl, MenuItem, Select } from '@mui/material';
 import { RiCamera3Line } from 'react-icons/ri';
+import BaseInput from 'components/input';
+import PrimaryLink from 'components/link/primary';
+import PrimaryLink2 from 'components/link/primary/variant/soft';
 
 
 const ProfileSetting = () => {
@@ -12,7 +15,6 @@ const ProfileSetting = () => {
     last_name: '',
     email: '',
     gender: '',
-    password: '',
     address: '',
     zip_code: '',
     profile_image: null
@@ -94,7 +96,7 @@ const ProfileSetting = () => {
 
             <div className='absolute bottom-0 left-0 z-[2] flex justify-center w-full py-[0.5rem] bg-black/60'>
               <label htmlFor='profile_image' className='relative w-full h-full'>
-                <RiCamera3Line className='text-white text-[1.5rem] ' />
+                <RiCamera3Line className='text-white text-[1.5rem] mx-auto ' />
                 <input
                   id='profile_image' 
                   type='file'
@@ -105,66 +107,71 @@ const ProfileSetting = () => {
               </label>
             </div>
           </div>
-          <form onSubmit={handleSubmit} className='grid grid-cols-1 xmd:grid-cols-2 md:max-lg:grid-cols-1 md:grid-cols-2 gap-[1rem] gap-y-0 w-full'>
-          <FormControl fullWidth margin="normal">
-              <TextField 
-                label="First Name"
-                name="first_name"
+          <form onSubmit={handleSubmit} className='grid grid-cols-1 xmd:grid-cols-2 md:max-lg:grid-cols-1 md:grid-cols-2 gap-[1rem] gap-y-[2rem] w-full'>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='first_name' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >First Name</label>
+            
+              <BaseInput 
+                id='first_name'
+                name='first_name'
+                type='text'
                 value={form.first_name}
                 onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
-                required
+                placeholder='e.g John'
               />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField 
-                label="Last Name"
-                name="last_name"
+            </div>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='last_name' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >Last Name</label>
+            
+              <BaseInput 
+                id='last_name'
+                name='last_name'
+                type='text'
                 value={form.last_name}
                 onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
-                required
+                placeholder='e.g Doe'
               />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField 
-                label="Email"
-                name="email"
-                type="email"
+            </div>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='email' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >Email</label>
+            
+              <BaseInput 
+                id='email'
+                name='email'
+                type='email'
                 value={form.email}
                 onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
-                required
+                placeholder='e.g example@yahoo.com'
               />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
+            </div>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='email' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >Email</label>
               <Select
                 name="gender"
                 value={form.gender}
                 onChange={handleChange}
                 sx={{
-                  height: '3rem',
+                  height: '2.875rem',
+                  borderRadius: '12px',
+                  borderColor: 'rgba(119,126,144,0.3)',
+                  '&:hover .MuiOutlinedInput-notchedOutline': {
+                    borderColor: 'rgba(119,126,144,0.3)',
+                  },
+                  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                    borderColor: 'rgba(119,126,144,0.8)',
+                  },
                   '& .MuiSelect-select': {
                     height: '1.5rem',
                     display: 'flex',
@@ -179,62 +186,50 @@ const ProfileSetting = () => {
                 <MenuItem value="female">Female</MenuItem>
                 <MenuItem value="other">Other</MenuItem>
               </Select>
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField 
-                label="Password"
-                name="password"
-                type="password"
-                value={form.password}
-                onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
-                required
-              />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField 
-                label="Address"
-                name="address"
+            </div>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='address' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >Address</label>
+            
+              <BaseInput 
+                id='address'
+                name='address'
+                type='text'
                 value={form.address}
                 onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
+                placeholder='e.g St. 13 Angola - Nivea'
               />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField 
-                label="Zip Code"
-                name="zip_code"
+            </div>
+            <div className='flex flex-col gap-[0.5rem] '>
+              <label 
+                htmlFor='zip_code' 
+                className='text-black-fade text-[0.75rem] uppercase font-[600] '
+              >Zip code</label>
+            
+              <BaseInput 
+                id='zip_code'
+                name='zip_code'
+                type='number'
                 value={form.zip_code}
                 onChange={handleChange}
-                InputProps={{
-                  sx: {
-                    height: '3rem',
-                    '& input': {
-                      height: '1.5rem',
-                    },
-                  },
-                }}
+                placeholder='e.g 345743'
               />
-            </FormControl>
+            </div>
             <button type='submit' hidden ></button>
           </form>
-          <Button onClick={handleSubmit} variant="contained" color="primary" sx={{height: '3rem'}} fullWidth>
-            {loading? <div className="loader"></div> : "Update"}
-          </Button>
+          <div className='flex justify-end items-center gap-[1rem]'>
+            <PrimaryLink2 
+              width='max-content' 
+              // onClick={handleSubmit}
+            >Reset</PrimaryLink2>
+            <PrimaryLink width='max-content' onClick={handleSubmit}>
+              <div className='flex items-center gap-[0.5rem]'>
+                {loading? <div className="loader"></div> : "Update"}
+              </div>
+            </PrimaryLink>
+          </div>
         </div>
         
       </div>
