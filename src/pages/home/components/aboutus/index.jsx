eec9350b9027d4ace76d3caa718ceaf0fd89dca7
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-12 12:24:46 +0100

    created our teams section

--- src/pages/home/components/aboutus/index.jsx
+++ src/pages/home/components/aboutus/index.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import PrimaryButton from 'components/button/primary';
 import PrimaryLink from 'components/link/primary';
+import SubHeader from 'components/typography/sub_header';
 
 const About = () => {
   return (
-    <div className='flex flex-col items-start gap-[3rem] w-full py-[2rem] '>
-      <div className='flex flex-col xlg:flex-row justify-between gap-[1rem] w-full h-max '>
+    <div className='flex flex-col items-start gap-[2rem] w-full py-[2rem] '>
+      <SubHeader>About Us</SubHeader>
+      <p className='text-[1.15rem] text-gray-500 font-[600] text-start'>A professional event hosting platform for all events!</p>
+      {/* <div className='flex flex-col xlg:flex-row justify-between gap-[1rem] w-full h-max '>
         <div className='relative z-[2] flex flex-col gap-[1rem] w-full py-[2rem] px-[1rem] rounded-[12px] '>
           <h2 className='relative z-[1] text-[2.5rem] text-black-dim font-[600] '>About Us</h2>
           <p className='relative z-[1] text-[1rem] text-gray-500 '>A professional event hosting platform for all events</p>
           {/* ::::::::::::::::::: overlay */}
-          <img 
+          {/* <img 
             src='/assets/images/glitters.jpg'
             alt='Dance floor'
             className='absolute top-0 left-0 opacity-[0.1] w-full h-full rounded-[16px] object-cover'
           />
         </div>
-      </div>
+      </div> */}
 
-      <div className='flex flex-col xlg:flex-row justify-between gap-[1rem] w-full h-max '>
+      <div className='flex flex-col xlg:flex-row justify-between gap-[1rem] w-full h-max mt-[1rem] '>
         {/* ::::::::::::::::::: image */}
         <img 
           src='/assets/images/dance-beat-floor.jpg'
