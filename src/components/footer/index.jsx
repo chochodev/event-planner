387991abe8ac57ccff067b290c5ef73e5a24a88e
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-07-12 18:04:02 +0100

    updated the footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <Logo className='' />
+            <Logo className='w-[6.5rem] md:w-[8rem]' />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
