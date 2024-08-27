c1ac492afe3085500a27ef3de9e1b646621c4946
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-27 20:27:38 +0100

    bugs with the relative import method

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      assets: "/src/assets",
+      context: "/src/context",
+      routes: "/src/routes",
+    },
+  },
 })
