b0809a74cd3210f4621151e80946c00007abb27e
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-31 21:47:21 +0100

    initialized vite app without ts. Js configured

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-        alias: {
-            components: "/src/components",
-            assets: "/src/assets",
-            context: "/src/context",
-            routes: "/src/routes",
-            utils: "/src/utils",
-            pages: "/src/pages",
-            store: "/src/zustand",
-        },
-    }
-});
+  plugins: [react()],
+})
