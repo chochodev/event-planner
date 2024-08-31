6b586b26ad195faa8091066d65ab61856c0d3566
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-31 21:50:25 +0100

    update: absolute import

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      assets: "/src/assets",
+      context: "/src/context",
+      routes: "/src/routes",
+      utils: "/src/utils",
+      pages: "/src/pages",
+      store: "/src/zustand",
+    },
+  }
 })
