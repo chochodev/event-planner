df5aed950906513a0b553c0a3a4fe4fc316d83d8
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-11-01 07:49:33 +0100

    update: stroke color working fine

--- src/types/fabric-types.ts
+++ src/types/fabric-types.ts
@@ -1,10 +1,9 @@
-import { Object as FabricObject, IObjectOptions, Pattern, Gradient } from 'fabric/fabric-impl';
+import { Object as FabricObject, IObjectOptions } from 'fabric/fabric-impl';
 
 export interface CustomFabricObject extends FabricObject {
   radius?: number;
   fontSize?: number;
   text?: string;
-  stroke: string | undefined;
 }
 
 export type UpdateableProperties = Partial<IObjectOptions> & {
